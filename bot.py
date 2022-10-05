4b635b73f5f6ffe1afd0770bfac95ada200a2146
HamishAitken <72395263+HamishAitken@users.noreply.github.com>
2022-10-05 19:37:40 +0100

    Ping change

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 
 import socket
 
+s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM) 
 
 HOST = "fc00:1337::17" #IP for the virtual machine
 PORT = 6667 #the port used
@@ -12,15 +13,16 @@ botnick = "bot"
 
 #Using AF_INET6 because the host is a string representing a hostname for IPV6
 #We use SOCK_STREAM because is a TCP protocol, and not a UDP
-with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as s:
+socket.socket(socket.AF_INET6, socket.SOCK_STREAM) 
     
-    s.connect((HOST,PORT))
-    s.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick + " " + botnick + "n", "UTF-8")) #Wset all the fields to the bot nickname
-    s.send(bytes("NICK "+ botnick +"n", "UTF-8")) # assign the nick to the bot
+s.connect((HOST,PORT))
 
-    def ping(): # respond to server Pings.  
-        s.send(bytes("PONG :pingisn", "UTF-8"))
+s.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick + " " + botnick + "n", "UTF-8")) #Wset all the fields to the bot nickname
+s.send(bytes("NICK "+ botnick +"n", "UTF-8")) # assign the nick to the bot
+
+def ping(): # respond to server Pings.  
+    s.send(bytes("PONG :pingisn", "UTF-8"))
   
-    data = s.recv(1024)
+data = s.recv(1024)
 
 print(f"Received {data!r}")
