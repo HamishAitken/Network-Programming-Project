41784459d948cdf031bf1b59b76a2acde6285536
2462760 <94140159+2462760@users.noreply.github.com>
2022-10-05 10:23:24 +0100

    Update client.py

--- client.py
+++ client.py
@@ -2,12 +2,23 @@
 
 import socket
 
+
 HOST = "fc00:1337::17" #IP for the virtual machine
 PORT = 6667 #the port used
+BOTNICK = "SuperBot"
 
+#Using AF_INET6 because the host is a string representing a hostname for IPV6
+#We use SOCK_STREAM because is a TCP protocol, and not a UDP
 with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as s:
+    #Equal line:
+    #mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #mysocket.connect(HOST, 6667)
     s.connect((HOST,PORT))
-    s.sendall(b"TRYING THE CLIENT")
+
+    #Identifying myself!
+    ircsock.send("Hello! I'm "+ BOTNICK +"\n")
+    #The b it's something about decoding or bytes
+    s.sendall(b"TRYING THE CLIENT") #Sending data to the socket
     data = s.recv(1024)
 
 print(f"Received {data!r}")
