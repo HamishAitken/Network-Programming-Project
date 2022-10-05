751b50b735addb69e12a69c4ad4bbd573382d1b5
HamishAitken <72395263+HamishAitken@users.noreply.github.com>
2022-10-05 19:00:39 +0100

    Nick

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import socket
 HOST = "fc00:1337::17" #IP for the virtual machine
 PORT = 6667 #the port used
 
+botnick = "bot"
 
 #Using AF_INET6 because the host is a string representing a hostname for IPV6
 #We use SOCK_STREAM because is a TCP protocol, and not a UDP
@@ -14,9 +15,12 @@ with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as s:
     #mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #mysocket.connect(HOST, 6667)
     s.connect((HOST,PORT))
+    s.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick + " " + botnick + "n", "UTF-8")) #Wset all the fields to the bot nickname
+    s.send(bytes("NICK "+ botnick +"n", "UTF-8")) # assign the nick to the bot
+
 
     #Identifying myself!
-    s.send(b"Hello! I'm SUPER BOT\n")
+    
     #The b it's something about decoding or bytes
     s.sendall(b"TRYING THE CLIENT\n") #Sending data to the socket
     data = s.recv(1024)
