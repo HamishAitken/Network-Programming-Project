98c794d9d8ea38d89c4a1ef5e9392b074ab587fb
HamishAitken <72395263+HamishAitken@users.noreply.github.com>
2022-10-05 19:28:57 +0100

    Ping change

--- bot.py
+++ bot.py
@@ -8,6 +8,9 @@ PORT = 6667 #the port used
 
 botnick = "bot"
 
+def ping(): # respond to server Pings.  
+    ircsock.send(bytes("PONG :pingisn", "UTF-8"))
+
 #Using AF_INET6 because the host is a string representing a hostname for IPV6
 #We use SOCK_STREAM because is a TCP protocol, and not a UDP
 with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as s:
@@ -16,13 +19,7 @@ with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as s:
     s.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick + " " + botnick + "n", "UTF-8")) #Wset all the fields to the bot nickname
     s.send(bytes("NICK "+ botnick +"n", "UTF-8")) # assign the nick to the bot
 
-    def ping(): # respond to server Pings.  
-    ircsock.send(bytes("PONG :pingisn", "UTF-8"))
-
-
-    #Identifying myself!
-    
-    
+  
     data = s.recv(1024)
 
 print(f"Received {data!r}")
