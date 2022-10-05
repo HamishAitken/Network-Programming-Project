f0ae0a7afa473b1e2bb56b0e6cd4b74800fd2b38
Tao <58590928+Taolimon@users.noreply.github.com>
2022-10-05 11:32:46 +0100

    Update server.py
    Added in the basic structure

--- server.py
+++ server.py
@@ -1,1 +1,28 @@
+import socket
+
+#for selftesting host = 'localhost'
+#for the project subsmission hots = "fc00:b33f::17"
+host = "fc00:b33f::17"
+port = 6667
+
+#To store the clients that connect to the server
+clients = []
+
+
+s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+s.bind(host, port)
+s.listen(5)
+
+while True:
+    connection, client_address = s.accept()
+    try:
+        print("")
+        while True:
+            print(f"received connection from {client_address}")
+            data = connection.recv()
+            if not data:
+                break
+    finally:
+        print("Closing server")
+        connection.close()
 
