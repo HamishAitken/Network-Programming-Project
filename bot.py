96bc1ee0b285fe9e906ca7aa0a853ffdeec2fc91
HamishAitken <72395263+HamishAitken@users.noreply.github.com>
2022-10-06 17:32:03 +0100

    Working bot

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import socket
 ircSocket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM) 
 
 def ping(): # respond to server Pings.  
-    ircSocket.send("PONG :pingis\n")
+    ircSocket.send(bytes("PONG :pingis\n","UTF-8"))
 
 HOST = "fc00:1337::17" #IP for the virtual machine
 PORT = 6667 #the port used
@@ -19,7 +19,7 @@ channel = "#test"
  
     
 ircSocket.connect((HOST,PORT))
-ircSocket.send(bytes("USER "+ botnick + "\n", "UTF-8")) #Wset all the fields to the bot nickname
+ircSocket.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick +" "+ botnick + "\n", "UTF-8")) #Wset all the fields to the bot nickname
 ircSocket.send(bytes("NICK "+ botnick +"\n", "UTF-8")) # assign the nick to the bot
 ircSocket.send(bytes("JOIN "+ channel +"\n", "UTF-8"))
 
@@ -28,7 +28,7 @@ while active:
 
     data = ircSocket.recv(2048)
 
-    data = data.strip (bytes("\n\r", "UTF-8"))
+    #data = data.strip (bytes("\n\r", "UTF-8"))
 
     if data.find (bytes("PING :", "UTF-8")) != -1:
         ping()
