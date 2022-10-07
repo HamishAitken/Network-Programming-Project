1ddee383efaca5e63864a83634c4bc28e8af26c5
HamishAitken <72395263+HamishAitken@users.noreply.github.com>
2022-10-07 19:39:57 +0100

    Added comments

--- bot.py
+++ bot.py
@@ -1,34 +1,36 @@
-#Trying to use the client
-
 import socket
 
 
-HOST = "fc00:1337::17" #IP for the virtual machine
-PORT = 6667 #the port used
+HOST = "fc00:1337::17" #IP for the server on Ubuntu virtual machine
+PORT = 6667 #port the IRC server is using
 
+#initialising variables for server communication
 botnick = "bot"
 channel = "#test"
+active = True
 ircSocket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-channelUserList[]
 
 
-def ping(): # respond to server Pings.  
+#ping function used to maintain a conection to the server by responding to pings
+def ping():  
     ircSocket.send(bytes("PONG :bot \n","UTF-8"))
 
 
+#joinChannel function to take a channel name and join a channel on the IRC server
 def joinChannel(channel):
     ircSocket.send(bytes("JOIN "+ channel +"\n", "UTF-8"))
 
 
- 
+#establishes connection to the server 
 ircSocket.connect((HOST,PORT))
-ircSocket.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick +" "+ botnick + "\n", "UTF-8")) #Wset all the fields to the bot nickname
-ircSocket.send(bytes("NICK "+ botnick +"\n", "UTF-8")) # assign the nick to the bot
+
+#sets the bots identity on the server
+ircSocket.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick +" "+ botnick + "\n", "UTF-8")) 
+ircSocket.send(bytes("NICK "+ botnick +"\n", "UTF-8"))
 
 joinChannel(channel)
 
-
-active = True
+#loop for the bot to receive information sent from the server and process it
 while active:
 
     data = ircSocket.recv(2048)
