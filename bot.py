b5056b7abc82b475cc5e96c8c435d8ddb164f53e
HamishAitken <72395263+HamishAitken@users.noreply.github.com>
2022-10-07 19:23:23 +0100

    Cleaned up code

--- bot.py
+++ bot.py
@@ -3,38 +3,40 @@
 import socket
 
 
-
-ircSocket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM) 
-
-def ping(): # respond to server Pings.  
-    ircSocket.send(bytes("PONG :pingis\n","UTF-8"))
-
 HOST = "fc00:1337::17" #IP for the virtual machine
 PORT = 6667 #the port used
 
 botnick = "bot"
 channel = "#test"
+ircSocket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+channelUserList[]
+
+
+def ping(): # respond to server Pings.  
+    ircSocket.send(bytes("PONG :bot \n","UTF-8"))
+
+
+def joinChannel(channel):
+    ircSocket.send(bytes("JOIN "+ channel +"\n", "UTF-8"))
 
 
  
-    
 ircSocket.connect((HOST,PORT))
 ircSocket.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick +" "+ botnick + "\n", "UTF-8")) #Wset all the fields to the bot nickname
 ircSocket.send(bytes("NICK "+ botnick +"\n", "UTF-8")) # assign the nick to the bot
-ircSocket.send(bytes("JOIN "+ channel +"\n", "UTF-8"))
+
+joinChannel(channel)
+
 
 active = True
 while active:
 
     data = ircSocket.recv(2048)
 
-    #data = data.strip (bytes("\n\r", "UTF-8"))
-
-    if data.find (bytes("PING :", "UTF-8")) != -1:
+    if data.find(bytes("PING :", "UTF-8")) != -1:
         ping()
 
-  
+     
 
 
 
-#print(f"Received {data!r}")
