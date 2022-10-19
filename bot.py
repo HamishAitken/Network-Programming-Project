12714fb861155faf9abb5e85c02625fe05c51f2e
HamishAitken <72395263+HamishAitken@users.noreply.github.com>
2022-10-19 21:51:51 +0100

    Added channel names

--- bot.py
+++ bot.py
@@ -13,22 +13,54 @@ ircSocket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 
 #ping function used to maintain a conection to the server by responding to pings
 def ping():  
-    ircSocket.send(bytes("PONG :bot \n","UTF-8"))
+    ircSocket.send(bytes("PONG :bot\r\n","UTF-8"))
 
 
-#joinChannel function to take a channel name and join a channel on the IRC server
-def joinChannel(channel):
-    ircSocket.send(bytes("JOIN "+ channel +"\n", "UTF-8"))
 
 
+    
 #establishes connection to the server 
 ircSocket.connect((HOST,PORT))
 
 #sets the bots identity on the server
-ircSocket.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick +" "+ botnick + "\n", "UTF-8")) 
-ircSocket.send(bytes("NICK "+ botnick +"\n", "UTF-8"))
+ircSocket.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick +" "+ botnick + "\r\n", "UTF-8")) 
+ircSocket.send(bytes("NICK "+ botnick +"\r\n", "UTF-8"))
 
-joinChannel(channel)
+#joins a channel and gets information on its users 
+Channel.joinChannel(channel)
+namesArray = Channel.getChannelUsers(channel)
+
+
+class Channel:
+
+    #joinChannel function to take a channel name and join a channel on the IRC server
+    def joinChannel(channel):
+        ircSocket.send(bytes("JOIN "+ channel +"\r\n", "UTF-8"))
+
+    def getChannelUsers(channel):
+        ircSocket.send(bytes("WHO "+ channel +"\r\n", "UTF-8"))
+        data = ircSocket.recv(2048)
+        data = data.decode('UTF-8')
+
+        userInfo = data.split("\r\n")
+
+        userNamesArray[len(userInfo-1)] 
+        i = 0
+
+        while userInfo.find(bytes(":End of WHO list", "UTF-8")) != -1:
+
+            userNamesArray[i] = userInfo[4]
+     
+            i = i + 1
+        
+        return userNamesArray
+    
+
+
+            
+
+
+
 
 #loop for the bot to receive information sent from the server and process it
 while active:
@@ -38,7 +70,7 @@ while active:
     if data.find(bytes("PING :", "UTF-8")) != -1:
         ping()
 
-     
+    
 
 
 
